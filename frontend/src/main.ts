6e5cfde29dc6c2fcd0b9c37ab1a305993142ab9a
Mateus Sachetti <mateus.sachetti@gmail.com>
2026-01-22 11:59:11 -0400

    Add keycloak authentication

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,34 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import keycloak from "./plugins/keycloak";
+
+keycloak
+  .init({
+    onLoad: "login-required", // or 'check-sso'
+    checkLoginIframe: false,
+    pkceMethod: "S256",
+  })
+  .then((authenticated) => {
+    if (!authenticated) {
+      window.location.reload();
+    } else {
+      const app = createApp(App);
+      app.config.globalProperties.$keycloak = keycloak;
+      // app.use(router)
+      app.mount("#app");
+
+      // Auto-refresh token
+      setInterval(() => {
+        keycloak.updateToken(70).catch(() => {
+          console.error("Failed to refresh token");
+        });
+      }, 60000);
+    }
+  })
+  .catch((error) => {
+    console.error("Authentication failed", error);
+  });
+
+//createApp(App).mount('#app')
